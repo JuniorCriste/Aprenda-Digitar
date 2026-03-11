e072bbe556acfa1a1f629269d5528a6458c0c79c
JuniorCriste <informaticode@gmail.com>
2026-03-11 15:51:41 -0300

    Aprenda Digitar

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const levels = [
     { name: "Nível 1: Letras Base", content: "asdfg hjklç" },
+    { name: "Nível 1: Letras Base", content: "qwert yuiop" },
+    { name: "Nível 1: Letras Base", content: "zxcvb nm" },
     { name: "Nível 2: Acentuação", content: "café maçã avô você" },
+    { name: "Nível 2: Acentuação", content: "moto FeLiCiDaDe arroz DJ" },
+    { name: "Nível 2: Acentuação", content: "Geografia variável anjo avó" },
     { name: "Nível 3: Frases Curtas", content: "O sol brilha para todos." },
-    { name: "Nível 4: Desafio Final", content: "A digitação rápida exige prática constante." }
+    { name: "Nível 3: Frases Curtas", content: "Eu uso Linux." },
+    { name: "Nível 3: Frases Curtas", content: "Trabalhei o dia inteiro. Estou exausta!" },
+    { name: "Nível 3: Frases Curtas", content: "sei lá..." },
+    { name: "Nível 4: Frases grandes", content: "A digitação rápida exige prática constante. O domínio vem através da disciplina!" },
+    { name: "Nível 4: Frases grandes", content: "Falaram que a imaginação é mais importante que o conhecimento, será?" },
+    { name: "Nível 4: Frases grandes", content: "Amanheceu ensolarado, mas o vento frio trouxe a melancolia e as núvens cinzentas de tristeza." },
+    { name: "Nível 4: Frases grandes", content: "Formatarei meu computador. Instalarei Linux! OO que acha, Pedro?" },
+    { name: "Nível 4: Frases grandes", content: "Sim, Bowie! Eu estou cara a cara com o homem que vendeu o mundo!" },
+    
 ];
 
 let currentLevelIdx = 0;
